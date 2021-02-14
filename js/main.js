45610864c6f580036db304eaea8f9011b4a93a57
Jeremy Francois <jeremy.francois@outlook.com>
2021-02-14 22:21:03 +0100

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+ $('.table').bootstrapTable();
+ $('.table').bootstrapTable('showLoading');
+ let arrows = [];
+ $.get('resources/victory.json', load);
+
+ function load(data){
+  
+  data.forEach(model => populate);
+  $('.table').bootstrapTable('hideLoading');
+  $('.table').bootstrapTable('load',arrows);
+ }
+
+ function populate(model){
+  model.specs.forEach(s => arrows.push($.extend({},model,s)));
+ }
+});
